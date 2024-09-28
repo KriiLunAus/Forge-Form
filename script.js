db40b4447d8d10b6b1e8e5e3a694780af8d511e6
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-09-28 21:26:46 +0300

    added modal on js

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
-const searchField = document.getElementById("inputField");
-const armoryList = document.querySelectorAll("#armoryList li");
+let items = [];
 
+//itemsColection fetch
+async function fetchItems() {
+  try {
+    const response = await fetch("./items_collection.json");
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+    const data = await response.json();
 
-searchField.addEventListener("input", (evt)=>{
-    const searchValue = evt.target.value.toLowerCase();
+    renderItems(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-    armoryList.forEach(item => {
-        if(item.textContent.toLowerCase().includes(searchValue)){
-            item.style.display = "";
-        }else{
-            item.style.display = "none"
-        }
-    });
-})
+// function for renderingItems
 
+function renderItems(data) {
+  const catalogSection = document.getElementById("catalogSection");
+  const cardWrapper = document.createElement("div");
+  const modal = document.createElement("div");
 
+  cardWrapper.className = "cardModalWrapper";
+  modal.className = "cardModal";
 
+  cardWrapper.appendChild(modal);
+  catalogSection.appendChild(cardWrapper);
+
+  const renderedData = data.longswords;
+
+  for (const category in renderedData) {
+    const swords = renderedData[category];
+    const listOfTiers = document.createElement("ul");
+    const tierHeader = document.createElement("h2");
+    listOfTiers.appendChild(tierHeader);
+
+    for (const sword of swords) {
+      const listElement = document.createElement("li");
+      const nameOfArnament = document.createElement("p");
+      const addToCartBtn = document.createElement("button");
+
+      tierHeader.textContent =
+        category.charAt(0).toUpperCase() + category.slice(1);
+      nameOfArnament.textContent = sword.name;
+      addToCartBtn.textContent = "Add to cart";
+
+      listElement.appendChild(nameOfArnament);
+      listElement.appendChild(addToCartBtn);
+
+      listOfTiers.appendChild(listElement);
+      modal.appendChild(listOfTiers);
+    }
+  }
+}
