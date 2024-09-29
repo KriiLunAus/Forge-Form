1a764bf97ca34f0fd2f54a5be5c4094280521a71
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-09-29 10:01:28 +0300

    added modal on click

--- script.js
+++ script.js
@@ -1,49 +1,60 @@
-let items = [];
-
 //itemsColection fetch
-async function fetchItems() {
+async function fetchItems(catalogName) {
   try {
     const response = await fetch("./items_collection.json");
     if (!response.ok) {
       throw new Error("Something went wrong!");
     }
     const data = await response.json();
-
-    renderItems(data);
+    renderItems(data, catalogName);
   } catch (error) {
     console.error(error);
   }
 }
 
 // function for renderingItems
+const catalogSection = document.getElementById("catalogSection");
+const closeModal = document.createElement("div");
+const cardWrapper = document.createElement("div");
 
-function renderItems(data) {
-  const catalogSection = document.getElementById("catalogSection");
-  const cardWrapper = document.createElement("div");
+function renderItems(data, catalogName) {
+  if (cardWrapper.parentNode) {
+    cardWrapper.parentNode.removeChild(cardWrapper);
+  }
+
+  const closeModalWrapper = document.createElement("div");
   const modal = document.createElement("div");
 
+  closeModal.innerHTML = "\u00D7";
+  closeModalWrapper.className = "closeModalWrapper";
   cardWrapper.className = "cardModalWrapper";
   modal.className = "cardModal";
 
+  modal.setAttribute("aria-hidden", "false"); // Modal is visible
+  cardWrapper.setAttribute("role", "dialog"); // Indicate it's a dialog
+
+  closeModalWrapper.appendChild(closeModal);
+  modal.appendChild(closeModalWrapper);
   cardWrapper.appendChild(modal);
   catalogSection.appendChild(cardWrapper);
-
-  const renderedData = data.longswords;
+  const renderedData = data[catalogName];
 
   for (const category in renderedData) {
-    const swords = renderedData[category];
+    const arnaments = renderedData[category];
+
     const listOfTiers = document.createElement("ul");
     const tierHeader = document.createElement("h2");
+
     listOfTiers.appendChild(tierHeader);
 
-    for (const sword of swords) {
+    for (const arnament of arnaments) {
       const listElement = document.createElement("li");
       const nameOfArnament = document.createElement("p");
       const addToCartBtn = document.createElement("button");
 
       tierHeader.textContent =
         category.charAt(0).toUpperCase() + category.slice(1);
-      nameOfArnament.textContent = sword.name;
+      nameOfArnament.textContent = arnament.name;
       addToCartBtn.textContent = "Add to cart";
 
       listElement.appendChild(nameOfArnament);
@@ -53,4 +64,27 @@ function renderItems(data) {
       modal.appendChild(listOfTiers);
     }
   }
+  modal.focus();
 }
+
+let modalIsOpen = false;
+
+catalogSection.addEventListener("click", (evt) => {
+  const catalogName = evt.target.textContent.trim().toLowerCase();
+  if (!modalIsOpen) {
+    fetchItems(catalogName);
+  }
+  modalIsOpen = true;
+});
+
+closeModal.addEventListener("click", () => {
+  modalIsOpen = false;
+
+  if (cardWrapper) {
+    cardWrapper.setAttribute("aria-hidden", "true"); // Set aria-hidden to true
+    const parent = cardWrapper.parentNode;
+    parent.removeChild(cardWrapper);
+  }
+});
+
+console.log(modalIsOpen);
