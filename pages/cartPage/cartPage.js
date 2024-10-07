619e0484b21e78e6ebeda494f6e30a3ef461709d
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-10-07 15:35:34 +0300

    added remove-from-cart-btn logic

--- pages/cartPage/cartPage.js
+++ pages/cartPage/cartPage.js
@@ -1,13 +1,16 @@
 const listOfItmInCart = document.getElementById("listOfItmInCart");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const arrOfSavedItems = JSON.parse(localStorage.getItem("itemsInCart"));
-  if (arrOfSavedItems) {
+  let arrOfSavedItems = JSON.parse(localStorage.getItem("itemsInCart"));
+  if (arrOfSavedItems.length !== 0) {
+    // adding element from storage
+
     arrOfSavedItems.forEach((element) => {
-      const elementOfCartList = document.createElement("li");
       const removeFromCartBtn = document.createElement("div");
+      const elementOfCartList = document.createElement("li");
       const removeBtnWrapper = document.createElement("div");
 
+      removeFromCartBtn.className = "removeBtn";
       removeBtnWrapper.className = "removeBtnWrapper";
       removeFromCartBtn.textContent = "\u00D7";
       elementOfCartList.textContent = element;
@@ -16,7 +19,29 @@ document.addEventListener("DOMContentLoaded", () => {
       elementOfCartList.appendChild(removeBtnWrapper);
 
       removeBtnWrapper.appendChild(removeFromCartBtn);
+
+      // removing list element(from page and storage)
+
+      removeFromCartBtn.addEventListener("click", () => {
+        elementOfCartList.remove();
+
+        arrOfSavedItems = arrOfSavedItems.filter((item) => {
+          return item !== element;
+        });
+        console.log(arrOfSavedItems);
+        localStorage.setItem("itemsInCart", JSON.stringify(arrOfSavedItems));
+        checkIfCartIsEmpty();
+      });
     });
-  } else {
+  } else if (listOfItmInCart) {
+    listOfItmInCart.textContent =
+      "There is no items in here, please add something.";
+  }
+
+  function checkIfCartIsEmpty() {
+    if (listOfItmInCart.children.length === 0) {
+      listOfItmInCart.textContent =
+        "There are no items in here, please add something.";
+    }
   }
 });
